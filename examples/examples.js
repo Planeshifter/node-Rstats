50a4e99b6fbcec86b14268dcb93ee31093cb4471
Planeshifter <pburckhardt@outlook.com>
2015-03-20 01:52:28 -0400

    [UPDATE] tests, jshint

--- examples/examples.js
+++ examples/examples.js
@@ -42,6 +42,6 @@ h.parseEvalQ("print(2+3)");
 
 // test for RJSONIO toJSON precision issue
 h.parseEvalQ("json = toJSON(1234687.796545568, digits=Inf)");
-json  = h.get('json');
+var json  = h.get('json');
 json = JSON.parse(json);
 console.log(json);
